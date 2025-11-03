200fa69048d95726daf939d3697b4cca32300fe0
Yifeng Li <charlesliyifeng@gmail.com>
2025-11-03 13:35:08 +0800

    Add rl_solver

--- solvers/rl_solver.py
+++ solvers/rl_solver.py
@@ -1,7 +1,10 @@
 import time
 import numpy as np
 from typing import List, Dict, Any, Tuple
+
 from .base_solver import BaseSolver
+from train.utils import total_distance
+from train.model import generate_route
 
 class RLSolver(BaseSolver):
     """
@@ -13,9 +16,8 @@ class RLSolver(BaseSolver):
     Space Complexity: O(n)
     """
     
-    def __init__(self, model):
+    def __init__(self):
         super().__init__("RL Solver")
-        self.model = model  # Pre-trained RL model
     
     def solve(self, distance_matrix: np.ndarray, locations: List[Dict[str, Any]]) -> Tuple[List[int], float]:
         """
@@ -34,32 +36,13 @@ class RLSolver(BaseSolver):
         if n <= 1:
             return [0], 0.0
         
-        # Prepare input for the RL model
-        state = self._prepare_state(distance_matrix, locations)
-        
         # Get predicted route from the RL model
-        route = self.model.predict(state)
+        route = generate_route(distance_matrix)
         
         # Calculate total distance
-        total_distance = sum(distance_matrix[route[i]][route[i+1]] for i in range(len(route)-1))
+        dist = total_distance(route, distance_matrix)
         
         self.solve_time = time.time() - start_time
         
-        return route, total_distance
+        return route, dist
     
-    def _prepare_state(self, distance_matrix: np.ndarray, locations: List[Dict[str, Any]]) -> Any:
-        """
-        Prepare the state representation for the RL model.
-        
-        Args:
-            distance_matrix: NxN matrix of distances between locations
-            locations: List of location dictionaries
-            
-        Returns:
-            State representation suitable for the RL model
-        """
-        # Example state preparation (can be customized based on model requirements)
-        return {
-            "distance_matrix": distance_matrix,
-            "locations": locations
-        }
